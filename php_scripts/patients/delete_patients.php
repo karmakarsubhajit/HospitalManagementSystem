45fd98ac374698d663f5fd094f328423f458551e
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 18:21:48 +0530

    added style for del patient

--- php_scripts/patients/delete_patients.php
+++ php_scripts/patients/delete_patients.php
@@ -9,26 +9,25 @@
 <html>
     <head>
         <title>Delete Patients </title>
+		<link rel="stylesheet" href="../styles/delete_emp.css">
     </head>
     <body>
         
- <h2>Delete Patients! </h2>
- <form method="post" action="remove_patient.php">									
-		<div>
+ <h2 class="main__heading">Delete Patients! </h2>
+ <form method="post" action="remove_patient.php">
+	<div class="form__container">
+		<div class="block">
 			<label for="patientID">
 				ID
 			</label>
 			<input type="text" name="Patient_ID"  placeholder="Enter Patient ID">
 		</div>
-
-		
-														
-	<button type="submit" name="submit" >
-		Delete Patient
-	</button>
-	</form>
-												
-
-    </body>
+							
+		<button id="submit__button" type="submit" name="submit" >
+			Delete Patient
+		</button>
+	</div>
+</form>
+</body>
 
 </html>
