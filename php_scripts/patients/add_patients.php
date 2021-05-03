0d1575ef6c64166584672e789e5fd88096613b88
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 18:12:37 +0530

    added style for add patients

--- php_scripts/patients/add_patients.php
+++ php_scripts/patients/add_patients.php
@@ -9,74 +9,74 @@
 <html>
     <head>
         <title>Add Patients </title>
+		<link rel="stylesheet" href="../styles/add_emp.css">
     </head>
     <body>
         
- <h2>Add Patients! </h2>
+ <h2 class="main__heading">Add Patients! </h2>
  <form method="post" action="insert_patient.php">		
-
-		<div>
+ 	<div class="form__container">
+		<div class="block">
 			<label for="patientID">
 				ID
 			</label>
 			<input type="text" name=" Patient_ID"  placeholder="Enter Patient ID">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientName">
 				Name
 			</label>
 			<input type="text" name="Name"  placeholder="Enter Patient Name">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientDOB">
 				DOB
 			</label>
 			<input type="text" name="DOB"  placeholder="Enter DOB">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientGender">
 				 Gender
 			</label>
 			<input type="text" name="Gender"  placeholder="Enter Gender">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientAddress">
 				Address
 			</label>
 			<input type="text" name="Address"  placeholder="Enter Address">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientEmail">
 				Email
 			</label>
 			<input type="text" name="Email"  placeholder="Enter Email">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientContact">
 				Contact No
 			</label>
 			<input type="text" name="Contact_No"  placeholder="Enter Contact No">
 		</div>
 
-        <div>
+        <div class="block">
 			<label for="patientMedicalHistory">
 				Medical History
 			</label>
 			<input type="text" name="Medical_History"  placeholder="Enter Medical History">
 		</div>
 														
-	<button type="submit" name="submit" >
-		Add Patient
-	</button>
-	</form>
-												
-
-    </body>
+		<button id="submit__button" type="submit" name="submit" >
+			Add Patient
+		</button>
+	</div>
+</form>
+</body>
 
 </html>
