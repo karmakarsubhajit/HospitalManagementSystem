949064a5aa0e70b13fd93499cf499717cec4d92d
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 23:25:17 +0530

    added style

--- login.php
+++ login.php
@@ -6,13 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
   
     <title>Hospital Management System</title>
+	<link rel="stylesheet" href="./php_scripts/styles/login2.css">
+
 </head>
 <body>
-    <div>
-        <h3>Welcome to Hospital Management System </h3>
+    <div class="container">
+        <h3 id="welcome">Welcome to Hospital Management System </h3> 
+        <!-- <h3 id="to"> to </h3>
+        <h3 id="hms"> Hospital Management System </h3> -->
         <form action="verify.php" method="post">
-            <input type="text" name="UserName" placeholder=" User Name" class="form-control mb-3">
-            <input type="password" name="Password" placeholder=" Password" class="form-control mb-3">
-            <button name="Login">Login</button>
+            <div class="form">
+                <input class="row" id="username" type="text" name="UserName" placeholder=" User Name" class="form-control mb-3">
+                <input class="row" id="password" type="password" name="Password" placeholder=" Password" class="form-control mb-3">
+                <button class="row" id="submit__button" name="Login">Login</button>
+            </div>
         </form>
     </div>
+</body>
+</html>
