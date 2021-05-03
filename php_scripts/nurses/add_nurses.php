0ab2af1f9c417c7720e6b06cfa3ac0e5186eed7c
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 18:06:44 +0530

    added style for add nurses

--- php_scripts/nurses/add_nurses.php
+++ php_scripts/nurses/add_nurses.php
@@ -10,26 +10,28 @@
 <html>
     <head>
         <title>Add Nurses </title>
+		<link rel="stylesheet" href="../styles/add_emp.css">
     </head>
     <body>
         
- <h2>Add Nurses! </h2>
- <form method="post" action="insert_nurse.php">									
-		<div>
+ <h2 class="main__heading">Add Nurses! </h2>
+ <form method="post" action="insert_nurse.php">	
+	<div class="form__container">
+		<div class="block">
 			<label for="nurseID">
 				ID
 			</label>
 			<input type="text" name="Nurse_ID"  placeholder="Enter Nurse ID">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="nurseName">
 				Name
 			</label>
 			<input type="text" name="Name"  placeholder="Enter Nurse Name">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="nurseDOB">
 				DOB
 			</label>
@@ -37,7 +39,7 @@
 		</div>
 
 
-		<div>
+		<div class="block">
 			<label for="nurseGender">
 				 Gender
 			</label>
@@ -45,33 +47,32 @@
 		</div>
 
 
-		<div>
+		<div class="block">
 			<label for="nurseAddress">
 				Address
 			</label>
 			<input type="text" name="Address"  placeholder="Enter Address">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="nurseEmail">
 				Email
 			</label>
 			<input type="text" name="Email"  placeholder="Enter Email">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="nurseContact">
 				Contact No
 			</label>
 			<input type="text" name="Contact_No"  placeholder="Enter Contact No">
 		</div>
 														
-	<button type="submit" name="submit" >
-		Add Nurse
-	</button>
-	</form>
-												
-
-    </body>
+		<button id="submit__button" type="submit" name="submit" >
+			Add Nurse
+		</button>
+	</div>
+</form>
+</body>
 
 </html>
