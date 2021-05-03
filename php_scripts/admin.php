d48854bdef27fe6a18b3d94c0914a94832cda1c2
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 20:20:44 +0530

    Added styles

--- php_scripts/admin.php
+++ php_scripts/admin.php
@@ -12,16 +12,24 @@
 <html lang="en">
 	<head>
 		<title>Admin</title>
+		<link rel="stylesheet" href="./styles/options.css">
     </head>
     <body>
-        <div>
+    <div class="options__container">
             <ul>
-                <li><span><a href="doctors/index.php"> Doctors </a></span></li>    
-                <li><span><a href="patients/index.php"> Patients </a></span></li>
-                <li><span><a href="nurses/index.php"> Nurses </a></span></li>
-                <li><span><a href ="appointments/index.php"> Appointments </a> </span> </li>
-                
-             </ul>   
+                <li> 
+                    <a href="doctors/index.php"><div class="option">Doctors</div></a>
+                </li>    
+                <li> 
+                    <a href="patients/index.php"><div class="option">Patients</div></a>
+                </li>
+                <li> 
+                    <a href="nurses/index.php"><div class="option">Nurses</div></a>
+                </li>
+                <li> 
+                    <a href="appointments/index.php"><div class="option">Appointments</div></a>
+                </li>
+            </ul>   
         </div>  
     </body>
 </html>
