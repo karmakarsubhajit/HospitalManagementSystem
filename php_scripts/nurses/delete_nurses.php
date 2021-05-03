4ff00e7da24b36ed9bd46dcaaa9bb0f62afafae3
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 18:19:10 +0530

    added style for del nurse

--- php_scripts/nurses/delete_nurses.php
+++ php_scripts/nurses/delete_nurses.php
@@ -9,26 +9,25 @@
 <html>
     <head>
         <title>Delete Nurses </title>
+		<link rel="stylesheet" href="../styles/delete_emp.css">
     </head>
     <body>
         
- <h2>Delete Nurses! </h2>
- <form method="post" action="remove_nurse.php">									
-		<div>
+ <h2 class="main__heading">Delete Nurses! </h2>
+ <form method="post" action="remove_nurse.php">
+	<div class="form__container">
+		<div class="block">
 			<label for="nurseID">
 				ID
 			</label>
 			<input type="text" name="Nurse_ID"  placeholder="Enter Nurse ID">
 		</div>
-
-		
-														
-	<button type="submit" name="submit" >
-		Delete Nurse
-	</button>
-	</form>
 												
-
-    </body>
+		<button id="submit__button" type="submit" name="submit" >
+			Delete Nurse
+		</button>
+	</div>
+</form>
+ </body>
 
 </html>
