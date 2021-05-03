eae250c85fc42dae48129d16ea4d1ae13efeb8e3
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-03 18:16:42 +0530

    added style for del doc

--- php_scripts/doctors/delete_doctors.php
+++ php_scripts/doctors/delete_doctors.php
@@ -9,26 +9,24 @@
 <html>
     <head>
         <title>Delete doctors </title>
+		<link rel="stylesheet" href="../styles/delete_emp.css">
     </head>
     <body>
         
- <h2>Delete doctors! </h2>
- <form method="post" action="remove_doctor.php">									
-		<div>
+ <h2 class="main__heading">Delete doctors! </h2>
+ <form method="post" action="remove_doctor.php">
+ 	<div class="form__container">
+		<div class="block">
 			<label for="doctorID">
 				ID
 			</label>
 			<input type="text" name="Doctor_ID"  placeholder="Enter Doctor ID">
-		</div>
-
-		
-														
-	<button type="submit" name="submit" >
-		Delete Doctor
-	</button>
-	</form>
-												
-
-    </body>
+		</div>									
+		<button id="submit__button" type="submit" name="submit" >
+			Delete Doctor
+		</button>
+	</div>
+</form>
+</body>
 
 </html>
