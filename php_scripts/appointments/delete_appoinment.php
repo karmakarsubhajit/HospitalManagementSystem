ea4903dda2a4c29889f43d1564820b9a777fb82c
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-05 16:46:19 +0530

    added style here

--- php_scripts/appointments/delete_appoinment.php
+++ php_scripts/appointments/delete_appoinment.php
@@ -9,26 +9,25 @@
 <html>
     <head>
         <title>Delete Appointments </title>
+		<link rel="stylesheet" href="../styles/delete_emp.css">
     </head>
     <body>
         
- <h2>Delete Appointments! </h2>
- <form method="post" action="remove_appointment.php">									
-		<div>
+ <h2 class="main__heading">Delete Appointments! </h2>
+ <form method="post" action="remove_appointment.php">	
+ 	<div class="form__container">
+		<div class="block">
 			<label for="AppID">
 				Appointment ID
 			</label>
 			<input type="text" name="App_ID"  placeholder="Enter Appointment ID">
 		</div>
-
-		
-														
-	<button type="submit" name="submit" >
-		Delete Appointment
-	</button>
-	</form>
-												
-
-    </body>
+										
+		<button id="submit__button" type="submit" name="submit" >
+			Delete Appointment
+		</button>
+	</div>
+</form>
+</body>
 
 </html>
