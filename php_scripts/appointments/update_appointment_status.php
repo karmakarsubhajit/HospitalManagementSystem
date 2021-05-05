33493db3e77c347b7c7fea5f0eca26c524d811e4
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-05 16:44:15 +0530

    added style here

--- php_scripts/appointments/update_appointment_status.php
+++ php_scripts/appointments/update_appointment_status.php
@@ -9,30 +9,31 @@
 <html>
     <head>
         <title>Uodate Appointment Status </title>
+		<link rel="stylesheet" href="../styles/update_emp.css">
     </head>
     <body>
         
- <h2>Update Appointment Status! </h2>
- <form method="post" action="update_appointmentDetails.php">									
-		<div>
+ <h2 class="main__heading">Update Appointment Status! </h2>
+ <form method="post" action="update_appointmentDetails.php">		
+ 	<div class="form__container">
+		<div class="block">
 			<label for="appID">
 				Appointment ID
 			</label>
 			<input type="text" name="App_ID"  placeholder="Enter Appointment ID">
 		</div>
-        <div>
+        <div class="block">
             <label for="status">
                 Appointment Status
             </label>
             <input type ="text" name ="status" placeholder="Enter Appointment Status">
         </div>
 												
-	<button type="submit" name="submit" >
-		Update Appointment Status
-	</button>
-	</form>
-												
-
-    </body>
+        <button id="submit__button" type="submit" name="submit" >
+            Update Appointment Status
+        </button>
+    </div>
+</form>
+</body>
 
 </html>
