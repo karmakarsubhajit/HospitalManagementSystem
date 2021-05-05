1503af50f3beaab896af449733f0047d6cc26e31
vishalrao487 <111801046@smail.iitpkd.ac.in>
2021-05-05 16:24:12 +0530

    added style here

--- php_scripts/appointments/book_appointments.php
+++ php_scripts/appointments/book_appointments.php
@@ -9,47 +9,46 @@
 <html>
     <head>
         <title>Book Appointments </title>
+		<link rel="stylesheet" href="../styles/add_emp.css">
     </head>
     <body>
         
- <h2>Book Appointments!</h2>
- <form method="post" action="insert_appointment.php">		
-        <div>
+ <h2 class="main__heading">Book Appointments!</h2>
+ <form method="post" action="insert_appointment.php">	
+ 	<div class="form__container">
+        <div class="block">
 			<label for="appID">
 			  Appointment ID
 			</label>
 			<input type="text" name="App_ID"  placeholder="Enter Appointment ID">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="patientID">
 			  Patient ID
 			</label>
 			<input type="text" name="Patient_ID"  placeholder="Enter Patient ID">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="doctorID">
 				Doctor ID
 			</label>
 			<input type="text" name="Doctor_ID"  placeholder="Enter Doctor ID">
 		</div>
 
-		<div>
+		<div class="block">
 			<label for="status">
 				status
 			</label>
 			<input type="text" name="status"  placeholder="Enter status">
-		</div>
+		</div>						
 
-	
-														
-	<button type="submit" name="submit" >
-		Book 
-	</button>
-	</form>
-												
-
-    </body>
+		<button id="submit__button" type="submit" name="submit" >
+			Book 
+		</button>
+	</div>
+</form>
+</body>
 
 </html>
